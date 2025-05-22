325b5d8b878a2f04f76aec9c0cbd443b495af7b8
Uwe van Heesch <u.vanheesch@bildungsinnovator.com>
2025-05-22 12:57:31 +0200

    Linter

--- src/main/java/net/vanheesch/better/SentenceGenerator.java
+++ src/main/java/net/vanheesch/better/SentenceGenerator.java
@@ -7,40 +7,40 @@ import java.util.random.RandomGenerator;
 
 public class SentenceGenerator {
 
-    public static final int NUMBER_OF_SENTENCES = 5;
+	public static final int NUMBER_OF_SENTENCES = 5;
 
-    private final List<String> subjects;
-    private final List<String> verbs;
-    private final List<String> objects;
-    private final RandomGenerator random;
+	private final List<String> subjects;
+	private final List<String> verbs;
+	private final List<String> objects;
+	private final RandomGenerator random;
 
-    public static void main(String[] args) {
-        List<String> subjects = Arrays.asList("Liam", "Priya", "Mila", "Amina", "Noah", "Horst", "Emilia", "Finn", "Samir", "Lea", "Ben", "Sofia");
-        List<String> verbs = Arrays.asList("tanzt", "codet", "bastelt", "zockt", "liebt", "hört", "sammelt", "zerlegt");
-        List<String> objects = Arrays.asList("Bluetooth-Boxen", "Avocados", "TikTok-Videos", "Sneaker", "LED-Stripes", "Proteinriegel", "Plastikroboter", "Regenjacken");
-
-        SentenceGenerator sentenceGenerator = new SentenceGenerator(new Random(), subjects, verbs, objects);
-
-        for (int i = 0; i < NUMBER_OF_SENTENCES; i++) {
-            System.out.println(sentenceGenerator.generateSentence());
-        }
-    }
-
-    public SentenceGenerator(RandomGenerator random, List<String> subjects, List<String> verbs, List<String> objects) {
-        this.subjects = subjects;
-        this.verbs = verbs;
-        this.objects = objects;
+	public SentenceGenerator(RandomGenerator random, List<String> subjects, List<String> verbs, List<String> objects) {
+		this.subjects = subjects;
+		this.verbs = verbs;
+		this.objects = objects;
 		this.random = random;
 	}
 
-    public String generateSentence() {
-        String subject = getRandomElement(subjects);
-        String verb = getRandomElement(verbs);
-        String object = getRandomElement(objects);
-        return String.format("%s %s %s.", subject, verb, object);
-    }
+	public static void main(String[] args) {
+		List<String> subjects = Arrays.asList("Liam", "Priya", "Mila", "Amina", "Noah", "Horst", "Emilia", "Finn", "Samir", "Lea", "Ben", "Sofia");
+		List<String> verbs = Arrays.asList("tanzt", "codet", "bastelt", "zockt", "liebt", "hört", "sammelt", "zerlegt");
+		List<String> objects = Arrays.asList("Bluetooth-Boxen", "Avocados", "TikTok-Videos", "Sneaker", "LED-Stripes", "Proteinriegel", "Plastikroboter", "Regenjacken");
 
-    private String getRandomElement(List<String> list) {
-        return list.get(random.nextInt(list.size()));
-    }
+		SentenceGenerator sentenceGenerator = new SentenceGenerator(new Random(), subjects, verbs, objects);
+
+		for (int i = 0; i < NUMBER_OF_SENTENCES; i++) {
+			System.out.println(sentenceGenerator.generateSentence());
+		}
+	}
+
+	public String generateSentence() {
+		String subject = getRandomElement(subjects);
+		String verb = getRandomElement(verbs);
+		String object = getRandomElement(objects);
+		return String.format("%s %s %s.", subject, verb, object);
+	}
+
+	private String getRandomElement(List<String> list) {
+		return list.get(random.nextInt(list.size()));
+	}
 }
